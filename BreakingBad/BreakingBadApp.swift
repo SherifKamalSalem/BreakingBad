0e20c4599627540cb2c5b090a2c8cf535a1e187b
Sherif Kamal <sherif.kamal@startappz.com>
2022-08-26 19:16:40 +0200

    Initial Commit

---
+++ BreakingBad/BreakingBadApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreakingBadApp.swift
+//  BreakingBad
+//
+//  Created by Sherif Kamal on 26/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct BreakingBadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

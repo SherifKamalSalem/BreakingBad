0b3d4125573353e230dce980e0809dfbd786c989
Sherif Kamal <sherif.kamal.salem@gmail.com>
2022-08-27 05:29:45 +0200

    Create `CharactersService` as default implementation for `Serviceable` interface

--- BreakingBad/Network/Serviceable.swift
+++ BreakingBad/Network/Serviceable.swift
@@ -11,3 +11,21 @@ import Combine
 protocol Serviceable {
     func getCharacters() -> AnyPublisher<Characters, NetworkError>
 }
+
+class CharactersService: Serviceable {
+    
+    private var networkRequest: Requestable
+    private var environment: Environment = .development
+    
+  // inject this for testability
+    init(networkRequest: Requestable, environment: Environment) {
+        self.networkRequest = networkRequest
+        self.environment = environment
+    }
+    
+    func getCharacters() -> AnyPublisher<[Character], NetworkError> {
+        let endpoint = ListServiceEndpoints.getCharacters
+        let request = endpoint.createRequest(environment: self.environment)
+        return self.networkRequest.request(request)
+    }
+}

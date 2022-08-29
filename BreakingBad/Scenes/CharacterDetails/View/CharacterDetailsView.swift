f9cde5f0e82d5acf1d321fe20a5093a924c8ce87
Sherif Kamal <sherif.kamal.salem@gmail.com>
2022-08-30 01:21:34 +0200

    Create `CharacterDetailsView` scene view

---
+++ BreakingBad/Scenes/CharacterDetails/View/CharacterDetailsView.swift
@@ -0,0 +1,28 @@
+//
+//  CharacterDetailsView.swift
+//  BreakingBad
+//
+//  Created by Sherif Kamal on 29/08/2022.
+//
+
+import SwiftUI
+
+struct CharacterDetailsView: View {
+    
+    @ObservedObject var viewModel: CharacterRowViewModel
+    
+    var body: some View {
+        NavigationView {
+            VStack(alignment: .center) {
+                AsyncImage(url: viewModel.imageURL)
+                    .frame(width: 300, height: 400).cornerRadius(4)
+                    .cornerRadius(8)
+                Text(viewModel.name).font(.title).foregroundColor(.black).bold()
+                Text(viewModel.nickname).font(.headline).foregroundColor(.gray)
+                Text(viewModel.category.rawValue).font(.subheadline).foregroundColor(.gray)
+                Text(viewModel.birthday).font(.subheadline).foregroundColor(.gray)
+            }
+        }
+        .navigationTitle(viewModel.name)
+    }
+}
